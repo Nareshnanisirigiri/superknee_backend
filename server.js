e491585927e3b752b96b983d61a6c8450ddce272
Naresh Nani Sirigiri <nareshani317@mail.com>
2026-03-06 22:30:04 +0530

    fix manifest

--- server.js
+++ server.js
@@ -29,8 +29,7 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // handle preflight requests
+app.use(cors(corsOptions)); // automatically handles preflight requests
 
 app.use(express.json());
 
@@ -46,12 +45,12 @@ app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
+// Dashboard routes
+app.use("/api/dashboard", dashboardRoutes);
+
 /* ======================
    SERVER
 ====================== */
-
-app.use("/api/dashboard", dashboardRoutes);
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
